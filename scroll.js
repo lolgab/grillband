c0bfdc5acb7330bd9f2e8f2435ccf80daa35bce7
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2025-08-05 13:52:27 +0200

    .

--- scroll.js
+++ scroll.js
@@ -1,12 +1,13 @@
 // Nascondi header quando si scrolla verso il basso, mostra quando si scrolla verso l'alto
 let lastScrollY = window.scrollY;
 let ticking = false;
+const ySize = 320;
 window.addEventListener('scroll', function () {
   if (!ticking) {
     window.requestAnimationFrame(function () {
       const header = document.querySelector('header');
       if (!header) return;
-      const topV = window.scrollY > 320 ? -320 : -window.scrollY
+      const topV = window.scrollY > ySize ? -ySize : -window.scrollY
       header.style.top = `${topV}px`;
       lastScrollY = window.scrollY;
       ticking = false;
